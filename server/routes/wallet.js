8a2b8bc6a1e9a80dcd1acb9b56e831feeb260749
Hollow Valley <goofylagaming@hotmail.com>
2026-09-18 13:29:39 +1000

    Preserve legacy wallet balance at cutover

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const { getWallet: getLegacyWallet } = require("../db");
 const { requireAuth } = require("../middleware/requireAuth");
 const automationRoutes = require("../../automation-platform/integration/liveRouteAdapters");
 
 const router = express.Router();
 
-router.get("/", requireAuth, (req, res) => automationRoutes.getWallet(req, res));
+router.get("/", requireAuth, (req, res) => automationRoutes.getWallet(req, res, {
+  legacyWallet: getLegacyWallet(req.user.id),
+}));
 
 module.exports = router;
