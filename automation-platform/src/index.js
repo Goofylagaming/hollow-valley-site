fd1869cc9fc5e663827637565e1c82de9b4a359d
Hollow Valley <goofylagaming@hotmail.com>
2026-09-18 22:10:49 +1000

    Mount daily login bonus routes

--- automation-platform/src/index.js
+++ automation-platform/src/index.js
@@ -12,6 +12,7 @@ const backupService = require('./services/backupService');
 const adminRoutes = require('./routes/adminRoutes');
 const websiteRoutes = require('./routes/websiteRoutes');
 const websiteAdminWalletRoutes = require('./routes/websiteAdminWalletRoutes');
+const websiteDailyLoginRoutes = require('./routes/websiteDailyLoginRoutes');
 const herbyBotRoutes = require('./routes/herbyBotRoutes');
 const bodyDropRoutes = require('./routes/bodyDropRoutes');
 const dinoStorageRoutes = require('./routes/dinoStorageRoutes');
@@ -159,6 +160,11 @@ app.use('/api/website/admin-wallet', (_req, res, next) => {
   res.set('Cache-Control', 'no-store');
   next();
 }, websiteAdminWalletRoutes);
+
+app.use('/api/website/daily-login', (_req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  next();
+}, websiteDailyLoginRoutes);
 
 app.use('/api/website', (_req, res, next) => {
   res.set('Cache-Control', 'no-store');
