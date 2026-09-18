f0aa560cf55a646df7d099f39d6c41ccb988072a
Hollow Valley <goofylagaming@hotmail.com>
2026-09-18 12:51:23 +1000

    Route wallet through automation economy

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const { getWallet } = require("../db");
 const { requireAuth } = require("../middleware/requireAuth");
+const automationRoutes = require("../../automation-platform/integration/liveRouteAdapters");
 
 const router = express.Router();
 
-router.get("/", requireAuth, (req, res) => {
-  res.json(getWallet(req.user.id));
-});
+router.get("/", requireAuth, (req, res) => automationRoutes.getWallet(req, res));
 
 module.exports = router;
