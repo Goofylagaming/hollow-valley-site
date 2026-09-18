6ab0ab69a4c740d04532a46018c19fab23e29923
Hollow Valley <goofylagaming@hotmail.com>
2026-09-18 19:21:58 +1000

    Remove temporary DinoStorage config probe runner

--- automation-platform/src/services/dinoStorageConfigProbeRunner.js
+++
@@ -1,6 +0,0 @@
-const { probeDinoStorageConfig } = require('./dinoStorageConfigProbe');
-setTimeout(() => {
-  probeDinoStorageConfig().catch((error) => {
-    console.log(`[dinostorage-config-probe] failed=${String(error?.code || error?.name || 'unknown')}`);
-  });
-}, 2500).unref?.();
