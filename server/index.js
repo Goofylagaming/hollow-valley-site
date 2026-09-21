cdb6512b0e1da287e6faa67d50a8da541a854b84
Hollow Valley <goofylagaming@hotmail.com>
2026-09-21 16:14:59 +1000

    Rebase admin Comms page and API

--- server/index.js
+++ server/index.js
@@ -29,6 +29,7 @@ const commandBridgeInternalRouter = require("./routes/commandBridgeInternal");
 const supporterInternalRouter = require("./routes/supporterInternal");
 const adminRestoreRouter = require("./routes/adminRestore");
 const adminOperationsRouter = require("./routes/adminOperations");
+const adminCommsRouter = require("./routes/adminComms");
 const serverStatusService = require("./services/serverStatus");
 const { syncSteamProfiles } = require("./services/steamProfile");
 const herbyBot = require("./herbyBot");
@@ -163,6 +164,7 @@ function createApp() {
   app.use("/api/events", eventsRouter);
   app.use("/api/admin-restore", adminRestoreRouter);
   app.use("/api/admin-operations", adminOperationsRouter);
+  app.use("/api/admin-comms", adminCommsRouter);
   app.use("/api/mapdata", mapdataRouter);
 
   app.all("/api/park", parkHandler);
